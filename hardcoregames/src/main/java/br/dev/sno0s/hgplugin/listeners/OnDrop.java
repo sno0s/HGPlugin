61358d1f09d84dcada790e5919923ac47f815773
sno0s <maathdesigner@outlook.com>
2025-09-23 21:05:05 -0300

    PlayerJoinEvent initial configurations

---
+++ hardcoregames/src/main/java/br/dev/sno0s/hgplugin/listeners/OnDrop.java
@@ -0,0 +1,33 @@
+package br.dev.sno0s.hgplugin.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class OnDrop implements Listener {
+
+    private final Set<String> blockedItems = new HashSet<>(Arrays.asList(
+            "§eSeletor de Kits"
+    ));
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        ItemStack item = event.getItemDrop().getItemStack();
+
+        if (item == null || !item.hasItemMeta()) return;
+
+        String displayName = item.getItemMeta().getDisplayName();
+
+        if (blockedItems.contains(displayName)) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("§cVocê não pode dropar " + displayName + "!");
+        }
+    }
+
+}
--- hardcoregames/src/main/java/br/dev/sno0s/hgplugin/listeners/PlayerJoinListener.java
+++ hardcoregames/src/main/java/br/dev/sno0s/hgplugin/listeners/PlayerJoinListener.java
@@ -1,11 +1,15 @@
 package br.dev.sno0s.hgplugin.listeners;
 
+import br.dev.sno0s.hgplugin.items.KitSelectorItem;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 public class PlayerJoinListener implements Listener {
 
@@ -13,8 +17,15 @@ public class PlayerJoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         World hgWorld = Bukkit.getWorld("hg_world"); // nome que você usou no WorldGeneration
         if (hgWorld != null) {
+            Player player = event.getPlayer();
             Location spawn = hgWorld.getSpawnLocation();
-            event.getPlayer().teleport(spawn);
+
+            //configs basicas
+            player.teleport(spawn); //teleporte ao spawn
+            player.setInvulnerable(true); // invencibilidade
+            player.getInventory().clear(); // cleanando inventario
+            player.getInventory().setItem(0, KitSelectorItem.create()); // setando seletor de kits
+
         }
     }
 }
